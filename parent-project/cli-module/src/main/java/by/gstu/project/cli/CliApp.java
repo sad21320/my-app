6c01e17905ad340c830fd5847e9ec91e8ee896ac
sad21320 <gerad.family@gmail.com>
2025-10-01 22:15:33 +0300

    Convert to multi-module project structure

---
+++ parent-project/cli-module/src/main/java/by/gstu/project/cli/CliApp.java
@@ -0,0 +1,10 @@
+package by.gstu.project.cli;
+
+import by.gstu.project.App;
+
+public class CliApp {
+    public static void main(String[] args) {
+        System.out.println("CLI Module: Calling core module...");
+        App.main(args);
+    }
+}
---
+++ parent-project/core-module/src/test/java/by/gstu/project/AppTest.java
@@ -0,0 +1,11 @@
+package by.gstu.project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
