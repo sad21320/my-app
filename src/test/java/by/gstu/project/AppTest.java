14e27f588fd9d5de3869b6bb7f822198e2c6d6d2
sad21320 <gerad.family@gmail.com>
2025-10-01 20:58:31 +0300

    Initial commit with basic project structure

---
+++ src/test/java/by/gstu/project/AppTest.java
@@ -0,0 +1,11 @@
+package by.gstu.project;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
